c452afd032c7139c8e9c8b4ad24fe996315366e5
Rohan Sakhale <rs@saiashirwad.com>
2017-01-09 22:19:19 +0530

    Updated cert

--- cert.sh
+++ cert.sh
@@ -1,4 +1,4 @@
 mkdir -p public/.well-known/acme-challenge/
-FOLDER_NAME="jHg7HWVkPn3K5URF2DNpaeqzwRGpC4aNm0NkOxW8G-s"
-FILE_CONTENT="jHg7HWVkPn3K5URF2DNpaeqzwRGpC4aNm0NkOxW8G-s.2a3gyVsastoMRtaSX0hkE0Ay27i3rl3elMzoQgKlO8Q"
+FOLDER_NAME="1ojK5v2vCt5b9WZ9iDTSi326R19VJ2e35ZR0PnYd-WE"
+FILE_CONTENT="1ojK5v2vCt5b9WZ9iDTSi326R19VJ2e35ZR0PnYd-WE.2a3gyVsastoMRtaSX0hkE0Ay27i3rl3elMzoQgKlO8Q"
 echo "$FILE_CONTENT" > "public/.well-known/acme-challenge/$FOLDER_NAME"

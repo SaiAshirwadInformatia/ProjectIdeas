71dbe473e595ccba0d8b1eccce77de2ce9a8f7a1
Rohan Sakhale <rs@saiashirwad.com>
2017-01-09 21:59:55 +0530

    Added SSL cert details and updated CI job

---
+++ cert.sh
@@ -0,0 +1,4 @@
+mkdir -p public/.well-known/acme-challenge/
+FOLDER_NAME="jHg7HWVkPn3K5URF2DNpaeqzwRGpC4aNm0NkOxW8G-s"
+FILE_CONTENT="jHg7HWVkPn3K5URF2DNpaeqzwRGpC4aNm0NkOxW8G-s.2a3gyVsastoMRtaSX0hkE0Ay27i3rl3elMzoQgKlO8Q"
+echo "$FILE_CONTENT" > "public/.well-known/acme-challenge/$FOLDER_NAME"
